54de126a2c84101ea4f9446410fb0e686046e76c
d-ivashchuk <dimaiv@me.com>
2023-04-21 12:56:02 +0200

    add correct lost pixel project id

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -5,6 +5,6 @@ export const config: CustomProjectConfig = {
   customShots: {
     currentShotsPath: "./lost-pixel",
   },
-  lostPixelProjectId: "clde9r3rh00v3m50vlq8y0k78",
+  lostPixelProjectId: "clgqfqifx0is5k80ef9a72utq",
   apiKey: process.env.LOST_PIXEL_API_KEY,
 };
